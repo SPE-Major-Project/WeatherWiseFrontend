006c6ffcaee1f1826ff15e56156bb96ee292bdb2
rishabh0014 <rishabhteli14@gmail.com>
2024-04-26 13:07:26 +0530

    Footer not fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-around md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <footer className="bottom-0 left-0 z-20 w-full mt-10 mb-0  bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-around md:p-6 dark:bg-gray-800 dark:border-gray-600">
       <div className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2023{" "}
         <a
